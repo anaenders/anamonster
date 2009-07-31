60e681fa95f72a1439bc6e60839fd9d275796ac0
Ian Enders <ian.enders@gmail.com>
2009-07-31 12:47:21 -0400

    using smtp.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,3 +22,6 @@ config.action_controller.perform_caching             = true
 
 # Disable delivery errors, bad email addresses will be ignored
 # config.action_mailer.raise_delivery_errors = false
+
+config.action_mailer.raise_delivery_errors = true
+ActionMailer::Base.delivery_method = :smtp
