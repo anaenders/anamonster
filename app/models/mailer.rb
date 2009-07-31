52ba8e4f2f7563f95004565cfb6b08ebbf2a5083
Ana Carter <anacarter@anas-macBook-2.local>
2009-07-31 13:04:33 -0400

    minor edit

--- app/models/mailer.rb
+++ app/models/mailer.rb
@@ -2,7 +2,7 @@ class Mailer < ActionMailer::Base
   def contact(contact)
     recipients 'aecarter@gmail.com'
     from       'contact@anamonster.com'
-    subject    "anamonster Contact from #{contact.name}"
+    subject    "anamonster contact from #{contact.name}"
     body       :contact => contact
   end
 end
