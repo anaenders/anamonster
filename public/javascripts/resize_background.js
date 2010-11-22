8aeaa1721b007f50a7ce804e53917763381b7288
Ana Carter <aecarter@gmail.com>
2010-11-22 01:42:04 -0500

    major ui edits

---
+++ public/javascripts/resize_background.js
@@ -0,0 +1,44 @@
+$(function() {
+  ResizingBackground.init();
+	ShowBackground.init();
+});
+
+var ShowBackground = function() {
+	
+	var backgroundFilePrefix = "/images/background";
+	var index = Math.floor(Math.random() * 7) + 1;
+	var img = backgroundFilePrefix + index + ".jpg"
+			
+	return {
+		init: function() {
+			var img = backgroundFilePrefix + index + ".jpg";
+			$('#background img').attr('src', img);
+			$('#background img').fadeIn();
+			$(window).trigger('ResizingBackground');
+		}
+	}
+}();
+
+var ResizingBackground = function() {
+
+	var BASE_WIDTH = 1000;
+	var BASE_HEIGHT = 667;
+	var WIDTH_FROM_HEIGHT = BASE_WIDTH / BASE_HEIGHT;
+	var HEIGHT_FROM_WIDTH = BASE_HEIGHT / BASE_WIDTH;
+	
+  return {    
+    init: function() {
+      $(window).resize(this.resize_background);
+      this.resize_background();
+    },
+    
+    resize_background: function() {
+			var current_width_ratio = $(window).width() / $(window).height();
+			if (current_width_ratio > WIDTH_FROM_HEIGHT) {
+				$('#background img').css({ width : $(window).width(), height : $(window).width() * HEIGHT_FROM_WIDTH });
+			} else {
+				$('#background img').css({ height : $(window).height(), width : $(window).height() * WIDTH_FROM_HEIGHT });
+			}
+    }
+  }
+}();
